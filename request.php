926c425c30fa676441226215d612f1011174ce3e
Siervo-Christine <2022310099@dhvsu.edu.ph>
2025-05-05 15:09:23 +0800

    itelec

--- request.php
+++ request.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
+    <title>Document Request</title>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="src/css/request.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
-
 </head>
 
 <body>
     <div class="wrapper">
+        <!-- SIDEBAR -->
         <aside id="sidebar">
             <div class="d-flex">
                 <button class="toggle-btn" type="button" title="Toggle Sidebar" aria-label="Toggle Sidebar">
@@ -64,8 +64,21 @@
             </ul>
         </aside>
 
+        <!-- MAIN CONTENT -->
+        <main class="main-content">
+            <div class="document-request-container">
+                <h2 class="document-title">DOCUMENT REQUEST</h2>
+                <div class="button-group">
+                    <a href="barangay_clearance.php" class="doc-button">Barangay Clearance</a>
+                    <a href="barangay_indigency.php" class="doc-button">Barangay Indigency</a>
+                    <a href="certificate_residency.php" class="doc-button">Certificate of Residency</a>
+                </div>
+                <a href="landingpage.php" class="cancel-link">cancel</a>
+            </div>
+        </main>
     </div>
 
+    <!-- SCRIPTS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
